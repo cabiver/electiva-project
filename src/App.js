da39cc9bd53a3e76a57d8aba72125e493cbafd48
cabiver <carlosvergel01@gmail.com>
2021-09-10 21:43:54 -0500

    add boostrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './App.css'
 import PrincipalPage from './components/PrincipalPage'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App () {
   return (
     <PrincipalPage/>
--- src/components/PrincipalPage.js
+++ src/components/PrincipalPage.js
@@ -1,20 +1,25 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
+import './principal-style.css'
 function PrincipalPage () {
-  const peticion = async () => {
-    const response = await fetch('/saludos')
-    if (response.ok) {
-      const jsonRes = await response.json()
-      const tex = document.getElementById('h1')
-      tex.innerText = `${tex.innerText} ${jsonRes.nose}`
-      console.log(jsonRes)
-    }
-  }
-  useEffect(() => {
-    peticion()
-  }, [])
+  // const peticion = async () => {
+  //   const response = await fetch('/saludos')
+  //   if (response.ok) {
+  //     const jsonRes = await response.json()
+  //     const tex = document.getElementById('h1')
+  //     tex.innerText = `${tex.innerText} ${jsonRes.nose}`
+  //     console.log(jsonRes)
+  //   }
+  // }
+  // useEffect(() => {
+  //   peticion()
+  // }, [])
   return (
+    <div className='first-page container'>
         <h1 id='h1'>hola pelotudos</h1>
+        <div className='bg-danger'> has odido pelotudo?</div>
+    </div>
+
   )
 }
 
